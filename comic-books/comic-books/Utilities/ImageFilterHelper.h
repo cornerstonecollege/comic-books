92a1c1fbda58b313e99aef9563effe038407325a
luizperes <luiz_peres10@hotmail.com>
2016-03-08 19:27:33 -0800

    Some effects were created :)

---
+++ comic-books/comic-books/Utilities/ImageFilterHelper.h
@@ -0,0 +1,28 @@
+//
+//  ImageFilterHelper.h
+//  comic-books
+//
+//  Created by CICCC1 on 2016-03-08.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageFilterHelper : NSObject
+
++ (instancetype) sharedInstance;
+
+- (UIImage *) hueImageWithImage:(UIImage *)img;
+- (UIImage *) colorClampImageWithImage:(UIImage *)img;
+- (UIImage *) crossPolynomialImageWithImage:(UIImage *)img;
+- (UIImage *) monochromeImageWithImage:(UIImage *)img;
+- (UIImage *) photoTonalImageWithImage:(UIImage *)img;
+- (UIImage *) lineOverlayImageWithImage:(UIImage *)img;
+- (UIImage *) CMYKHalftoneImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+- (UIImage *) dotScreenImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+- (UIImage *) hatchedScreenImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+- (UIImage *) hexPixellateImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+- (UIImage *) pixellateImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+- (UIImage *) pointillizeImageWithImage:(UIImage *)img andCenter:(CIVector *)center;
+
+@end
--- comic-books/comic-books/ViewController.m
+++ comic-books/comic-books/ViewController.m
@@ -7,16 +7,30 @@
 //
 
 #import "ViewController.h"
+#import "ImageFilterHelper.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgViewChanged;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self changeImage];
+}
+
+- (void) changeImage
+{
+    CGFloat centerX = self.imgView.bounds.origin.x + self.imgView.bounds.size.width / 2.0;
+    CGFloat centerY = self.imgView.bounds.origin.y + self.imgView.bounds.size.height / 2.0;
+    self.imgViewChanged.image = [[ImageFilterHelper sharedInstance] lineOverlayImageWithImage:self.imgView.image];
 }
 
 - (void)didReceiveMemoryWarning {
